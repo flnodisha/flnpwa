eff020e00ee4bcd3c46401ae36d6e1db5e63fa2d
flnodisha <odishafln@gmail.com>
2024-11-25 19:45:02 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,26 @@
-const CACHE_NAME = 'pwa-demo-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/manifest.json'
+const cacheName = 'fln-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/fln_logo.png',
+   '/1.png',
+   '2.png',
+    '3.png',
+  // Add other assets (icons, images)
 ];
 
-// Install event: cache static assets
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
 });
 
-// Fetch event: serve cached content or fetch from network
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-
-// Activate event: clean up old caches
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
